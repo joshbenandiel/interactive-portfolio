e2e15647e59e37102b07550238bcfcdf6db21c2b
joshbenandiel <joshjacinto22@gmail.com>
2022-05-20 13:51:17 +0800

    Try

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,18 +1,18 @@
 import { FC, useEffect, useState } from "react";
 import { BackgroundContainer } from "./styled"
 import backgroundlogo from '../Main/images/backgroundlogo.png'
-// import one from './images/1.png'
-// import two from './images/2.png'
-// import three from './images/3.png'
-// import four from './images/4.png'
-// import five from './images/5.png'
-// import six from './images/6.png'
-// import seven from './images/7.png'
-// import eight from './images/8.png'
-// import nine from './images/9.png'
-// import ten from './images/10.png'
-// import eleven from './images/11.png'
-// import twelve from './images/12.png'
+import one from './images/1.png'
+import two from './images/2.png'
+import three from './images/3.png'
+import four from './images/4.png'
+import five from './images/5.png'
+import six from './images/6.png'
+import seven from './images/7.png'
+import eight from './images/8.png'
+import nine from './images/9.png'
+import ten from './images/10.png'
+import eleven from './images/11.png'
+import twelve from './images/12.png'
 
 
 interface IProps {
@@ -50,7 +50,7 @@ export const Background: FC<IProps> = ({children, theme}) => {
     <BackgroundContainer theme={theme}>
       <>
       <img className='background-logo'src={backgroundlogo} alt="background" loading="lazy"/>
-      {/* <div className='layer-1'>
+      <div className='layer-1'>
         <img className='shapes' src={twelve} data-speed={-11} alt='shapes'/>
         <img className='shapes' src={ten} data-speed={10} alt='shapes'/>
         <img className='shapes' src={eleven} data-speed={8} alt='shapes'/>
@@ -69,7 +69,7 @@ export const Background: FC<IProps> = ({children, theme}) => {
         <img className='shapes' src={four} data-speed={-8} alt='shapes'/> 
         <img className='shapes' src={one} data-speed={-5} alt='shapes'/>
         <img className='shapes' src={three} data-speed={2} alt='shapes'/>
-      </div> */}
+      </div>
       {children}
       </>
     </BackgroundContainer>
