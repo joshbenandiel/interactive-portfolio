a41e4df46175d6aef8d721de519e5b205dc84a91
joshbenandiel <joshjacinto22@gmail.com>
2022-05-23 10:09:52 +0800

    test with main performance

--- src/Page/App.tsx
+++ src/Page/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { PreLoader } from "./styled"
 import { Navbar } from "../components/Navbar";
-// import { Main } from "../components/Main";
+import { Main } from "../components/Main";
 // import { Card } from '../components/Projects/Card'
 // import { Projects } from "../components/Projects";
 // import { index as Footer } from "../components/Footer";
@@ -23,10 +23,10 @@ const textColor = {
   light: '#4C4F65',
   dark: '#B0B2C3',
 }
-// const textColorMain = {
-//   light: '#4C4F65',
-//   dark: '#fff',
-// }
+const textColorMain = {
+  light: '#4C4F65',
+  dark: '#fff',
+}
 
 const textColorHover = {
   light: '#242526',
@@ -86,13 +86,13 @@ const App: React.FC = () => {
           techRef={techRef} 
           setOpenContact={setOpenContact}/>
       </div>
-      {/* <Main 
+      <Main 
         textColorMain={changeTheme ? textColorMain.light : textColorMain.dark}
         textColor={changeTheme ? textColor.light :  textColor.dark}
         textColorHover = {changeTheme ? textColorHover.light : textColorHover.dark}
         worksRef={worksRef}
         openContact={openContact} 
-        setOpenContact={setOpenContact}/> */}
+        setOpenContact={setOpenContact}/>
       </Background>
       {/* <Projects 
         worksRef={worksRef}
