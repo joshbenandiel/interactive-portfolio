5e64dd25eda0e2d7da1ea1ffc5e9f6fe6f4c675e
joshbenandiel <joshjacinto22@gmail.com>
2024-03-08 15:36:41 +0800

    revert

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,9 @@ import './index.css';
 import App from './Page/App';
 import reportWebVitals from './reportWebVitals';
 import {  BrowserRouter } from 'react-router-dom'
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
     <BrowserRouter>
